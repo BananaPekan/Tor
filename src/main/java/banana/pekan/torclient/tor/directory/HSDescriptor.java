8d66423ddcc998419c9bf0f312ae5c8eb5b6e03c
bananapekan <bananapekanlinux@gmail.com>
2025-03-03 15:23:25 +0200

    Parse second layer of encryption.

--- src/main/java/banana/pekan/torclient/tor/directory/HSDescriptor.java
+++ src/main/java/banana/pekan/torclient/tor/directory/HSDescriptor.java
@@ -12,6 +12,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class HSDescriptor {
 
@@ -20,6 +21,30 @@ public class HSDescriptor {
     static {
         try {
             SHA3_256 = MessageDigest.getInstance("SHA3-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    long revision;
+    byte[] hsPublicKey;
+    byte[] hsBlindedPublicKey;
+
+    public HSDescriptor(byte[] superencrypted, long revision, byte[] hsPublicKey, byte[] hsBlindedPublicKey) {
+        this.revision = revision;
+        this.hsPublicKey = hsPublicKey;
+        this.hsBlindedPublicKey = hsBlindedPublicKey;
+        try {
+            // SECRET_DATA = blinded-public-key
+            // STRING_CONSTANT = "hsdir-superencrypted-data"
+            String decryptedSuperencrypted = new String(decrypt(superencrypted, hsBlindedPublicKey, "hsdir-superencrypted-data"));
+            String beginWrapper = "encrypted\n-----BEGIN MESSAGE-----\n";
+            byte[] encrypted = Base64.getDecoder().decode(decryptedSuperencrypted.substring(decryptedSuperencrypted.indexOf(beginWrapper) + beginWrapper.length(), decryptedSuperencrypted.indexOf("\n-----END MESSAGE-----")).replaceAll("\n", ""));
+            // SECRET_DATA = blinded-public-key | descriptor_cookie
+            // (Note: since the client doesn't support restricted discovery at the moment, descriptor cookie is left blank)
+            // STRING_CONSTANT = "hsdir-encrypted-data"
+            byte[] decrypted = decrypt(encrypted, hsBlindedPublicKey, "hsdir-encrypted-data");
+            System.out.println(new String(decrypted));
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -40,28 +65,25 @@ public class HSDescriptor {
         return SHA3_256.digest();
     }
 
-    // WIP
-    public static void parseDirect(byte[] superencrypted, long revision, byte[] hsPublicKey, byte[] hsBlindedPublicKey) throws NoSuchAlgorithmException {
+    private byte[] decrypt(byte[] encrypted, byte[] SECRET_DATA, String STRING_CONSTANT) throws NoSuchAlgorithmException {
         byte[] credential = getCredential(hsPublicKey);
         byte[] subcredential = deriveSubCredential(credential, hsBlindedPublicKey);
 
         // secret_input = SECRET_DATA | N_hs_subcred | INT_8(revision_counter)
-        // In this case, SECRET_DATA is the hidden service's blinded public key.
         SHAKEDigest shakeDigest = new SHAKEDigest(256);
-        shakeDigest.update(hsBlindedPublicKey, 0, hsBlindedPublicKey.length);
+        shakeDigest.update(SECRET_DATA, 0, SECRET_DATA.length);
         shakeDigest.update(subcredential, 0, subcredential.length);
         shakeDigest.update(ByteBuffer.allocate(8).putLong(revision).array(), 0, 8);
 
         // keys = SHAKE256_KDF(secret_input | salt | STRING_CONSTANT, S_KEY_LEN + S_IV_LEN + MAC_KEY_LEN)
         byte[] salt = new byte[16];
-        System.arraycopy(superencrypted, 0, salt, 0, salt.length);
+        System.arraycopy(encrypted, 0, salt, 0, salt.length);
         byte[] mac = new byte[32];
-        int ciphertextLength = superencrypted.length - salt.length - mac.length;
+        int ciphertextLength = encrypted.length - salt.length - mac.length;
         byte[] ciphertext = new byte[ciphertextLength];
-        System.arraycopy(superencrypted, salt.length, ciphertext, 0, ciphertextLength);
-        System.arraycopy(superencrypted, salt.length + ciphertextLength, mac, 0, mac.length);
+        System.arraycopy(encrypted, salt.length, ciphertext, 0, ciphertextLength);
+        System.arraycopy(encrypted, salt.length + ciphertextLength, mac, 0, mac.length);
         shakeDigest.update(salt, 0, salt.length);
-        String STRING_CONSTANT = "hsdir-superencrypted-data";
         shakeDigest.update(STRING_CONSTANT.getBytes(), 0, STRING_CONSTANT.length());
 
 //       SECRET_KEY = first S_KEY_LEN bytes of keys
@@ -81,8 +103,7 @@ public class HSDescriptor {
             Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
             SecretKeySpec keySpec = new SecretKeySpec(secretKey, "AES");
             cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(secretIv));
-            byte[] decrypted = cipher.update(ciphertext);
-            System.out.println(new String(decrypted));
+            return cipher.update(ciphertext);
         } catch (NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException e) {
             throw new RuntimeException(e);
         }
